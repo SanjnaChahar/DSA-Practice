f5ff46317905672a15dd2865a2bc0ba21d83c113
Sanjna Chahar <sanjnachahar512@gmail.com>
2025-10-23 17:44:13 +0530

    Added solution for reversing words in a string problem

---
+++ Strings/reversewords.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+string result(string s){
+    int left = 0;
+    int right = s.length()-1;
+
+    string temp = "";
+    string ans = "";
+
+    while(left <= right){
+        char ch = s[left];
+        if(ch != ' '){
+            temp += ch;
+        }
+        else if(ch == ' '){
+            if(ans != "") ans = temp + " " + ans;
+            else ans = temp;
+            temp = "";
+        }
+        left++;
+    }
+
+    if(temp != ""){
+        if(ans != "") ans = temp + " " + ans;
+        else ans = temp;
+    }
+
+    return ans;
+}
+
